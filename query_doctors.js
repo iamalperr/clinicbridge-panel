533069be8592219d232bacc3c66e00c85e4dc1c8
Yusuf Alper Özgül <yusufalperozgul@172-2-0-117.lightspeed.dybhfl.sbcglobal.net>
2026-07-23 12:18:06 +0300

    Remove accidental query_doctors.js

--- query_doctors.js
+++
@@ -1,3 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Might not exist, let's use the default initialization if possible, or just skip if we don't have creds.
-// Wait, I can just use grep on the whole project to see if these names are in JSON files or seeder scripts.
